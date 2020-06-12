8c396996ba25139cd431a4a96e8ac0224163e3e3
sagarmachine <sagarmonkey800@gmail.com>
2020-06-13 00:18:21 +0530

    added state for products to be added

--- src/components/content/main/content/products/products-table-section/products-table-row/products-table-row.jsx
+++ src/components/content/main/content/products/products-table-section/products-table-row/products-table-row.jsx
@@ -1,28 +1,18 @@
 import React, {useState} from 'react'
 
- const ProductsTableRow=(props)=> {
-   let [myprice,setMyprice] = useState(props.product.myprice)
-   const myPriceHandler = (e)=>{
-      const newMyprice = e.target.value
-      setMyprice(newMyprice)
-   }
-   const checkBoxHandler = (e,productid)=>{
-     if(myprice===0){
-       console.log("0");
-     }else{
-       console.log(myprice);
-     }
-   }
-    return (
+
+
+const ProductsTableRow=(props)=>
+     (
         <tr className="products__table-body--row">
-          <td className="products__table-body--item"><input onClick={(e)=>checkBoxHandler(e,props.product.productid)} type="checkbox"/></td>
+          <td className="products__table-body--item"><input  id={"c"+props.product.productid} onClick={(event)=>props.checkBoxClick(event,props.product.productid)} type="checkbox"/></td>
           <td className="products__table-body--item">{props.product.productname}</td>
           <td className="products__table-body--item">{props.product.unit}</td>
           <td className="products__table-body--item">{props.product.price}</td>
-          <td className="products__table-body--item"><input onChange={myPriceHandler} type="tel" value={myprice}/></td>
+          <td className="products__table-body--item"><input  id={"p"+props.product.productid} onChange={(event)=>props.inputChange(event,props.product.productid)}  /></td>
         </tr>
     )
-}
+
 
 
 export default ProductsTableRow
--- src/components/content/main/content/products/products-table-section/products-table-section.jsx
+++ src/components/content/main/content/products/products-table-section/products-table-section.jsx
@@ -3,9 +3,72 @@ import ProductsTableRow from './products-table-row/products-table-row'
 
 
 export default class ProductsTableSection extends Component {
+
+
+  state={
+    addProducts:[]
+  }
+
+
+  inputChangeHandler=(event,productId)=>{
+
+    if(document.getElementById("c"+productId).checked){
+
+      let index= this.state.addProducts.findIndex(product=>product.productId===productId);
+      let newAddProducts=[...this.state.addProducts]
+      newAddProducts[index].myprice=event.target.value;
+      this.setState({
+        addProducts:newAddProducts
+      })
+    }
+
+
+  }
+
+
+  checkBoxHandler=(event,productId)=>{
+
+   
+    if(event.target.checked){
+       let s=[];
+    let price = document.getElementById("p"+productId).value;
+     if(price){}
+     else{
+      let index= this.props.products.findIndex(product=>product.productid===productId);
+       price=this.props.products[index].price;
+     }
+     let newProduct={
+       myprice:price,
+       productId:productId
+     }
+     let newAddProducts=[...this.state.addProducts, newProduct]
+     this.setState({
+       addProducts:newAddProducts
+     })
+
+    }
+    else {
+      let index= this.state.addProducts.findIndex(product=>product.productid===productId);
+      let newAddProducts=[...this.state.addProducts]
+      newAddProducts.splice(index,1);
+      this.setState({
+        addProducts:newAddProducts
+      })
+
+    }
+
+  }
+
+  getAddProducts=()=>{
+
+    this.state.addProducts.forEach(product=>{console.log(product.productId+":"+product.myprice)});
+
+  }
+
+
     render() {
         return (
-
+<React.Fragment>
             <table className="products__table">
                 <thead className="products__table-header">
                   <tr className="products__table-header--row">
@@ -17,10 +80,12 @@ export default class ProductsTableSection extends Component {
                   </tr>
                 </thead>
                 <tbody className="products__table-body">
-                {this.props.products.map(product=><ProductsTableRow product={product} />)}
+                {this.props.products.map(product=><ProductsTableRow product={product} checkBoxClick={this.checkBoxHandler} inputChange={this.inputChangeHandler}/>)}
                 </tbody>
 
             </table>
+            <button onClick={this.getAddProducts}>TEST</button>
+            </React.Fragment>
         )
     }
 }
