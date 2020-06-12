592ef97b5cce8ba491dfc8f29fb53c735a7b659b
sagarmachine <sagarmonkey800@gmail.com>
2020-06-12 18:14:18 +0530

    product connected to spring

---
+++ src/components/content/main/content/products/products-table-section/products-table-row/products-table-row.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const ProductsTableRow=(props)=> {
+    return (
+        <div>
+            product name  : {props.product.productname}
+        </div>
+    )
+}
+
+
+export default ProductsTableRow
--- src/components/content/main/content/products/products.jsx
+++ src/components/content/main/content/products/products.jsx
@@ -1,18 +1,71 @@
 import React from "react"
 import ProductsMenu from "./products-menu/products-menu";
+import Modal from "../../../../UI/modal/modal";
+import Backdrop from "../../../../UI/backdrop/backdrop";
+import axios from "axios";
+import Spinner from "../../../../UI/spinner/spinner";
+import LayOutContext from "../../../layout-context";
+import ProductsTableSection from "./products-table-section/products-table-section";
 
 
-const Products = ()=>{
-  return (
-    <React.Fragment>
-      <ProductsMenu/>
+
+
+class Products extends React.Component {
+
+
+  state={
+    loadingCategories:true,
+    loadingProducts:true,
+    products:null,
+    categories:null,
+    categorySelected:-1,
+  }
+
+  static contextType=LayOutContext;
+
+  componentDidMount(){
+    this.setState({loading:true})
+  }
+
+  componentDidUpdate(){
+    if(this.state.loadingCategories===true)
+    axios.get("http://localhost:7571/getProductCategoriesByUserSubServiceCode/"+this.context.sellerCode)
+    .then(data=> {this.setState({categories:data.data,loadingCategories:false});}
+      )
+
+      if(this.state.loadingProducts===true)
+      axios.get("http://localhost:7571/getProductsBySellerCode?sellercode="+this.context.sellerCode)
+    .then(data=> {this.setState({products:data.data,loadingProducts:false});}
+      )
+  }
+
+  render(){
+
+
+
+    let content=<Spinner/>;
+    let menu=null;
+
+    if(this.state.loadingCategories===false){
+    menu=  <ProductsMenu categories={this.state.categories}/>
+ }
+
+ if(this.state.loadingProducts===false){
+    content=( 
     <div className="products">
        <div className="products__table-header">
-        products__table
+         <ProductsTableSection products={this.state.products}/>
        </div>
     </div>
-    </React.Fragment>
+    )}
+
+  return (
+    <React.Fragment>
+    {menu}  
+   {content}
+   </React.Fragment>
   )
+}
 }
 
 export default Products
