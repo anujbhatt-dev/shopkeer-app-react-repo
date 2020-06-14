c368bd8f780937d05c15093f8486bacfb1a838c3
anuj bhatt <anujbhatt023@gmail.com>
2020-06-14 23:00:26 +0530

    added sidebar toggle

--- src/components/UI/sidebar.jsx
+++ src/components/UI/sidebar.jsx
@@ -1,10 +1,19 @@
-import React from "react"
+import React,{useState} from "react"
 
 const Sidebar = (props) =>{
+  const sideBarToggleHandler= (e)=>{
+      console.log(e.target.checked);
+  }
+
   return (
-    <div className="sidebar">
-      <button className="sidebar__btn"><span className="sidebar__btn-span"><i className="fa fa-bars" aria-hidden="true"></i></span></button>
+    <div className="sidebarFix">
+      <div  className="sidebar">
+        <input onClick={sideBarToggleHandler} id="sidebarId" type="checkbox" className="sidebar__toggle"/>
+        <div className="slideSideBar">hello</div>
+        <label for="sidebarId" className="sidebar__btn"><span className="sidebar__btn-span"><i className="fa fa-bars" aria-hidden="true"></i><span className="sidebar__btn-span2">8</span></span></label>
+      </div>
     </div>
+
   )
 }
 
